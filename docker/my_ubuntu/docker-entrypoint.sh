fbf7aaaa05dc48d950213625df1667aa0a2d98ce
PhatNguyen <phatnt.uit@gmail.com>
2020-05-13 14:11:25 +0700

    Update my_ubuntu docker

---
+++ docker/my_ubuntu/docker-entrypoint.sh
@@ -0,0 +1,9 @@
+#!/bin/bash -e
+
+echo "Starting Ubuntu Development environment..."
+
+echo "Started!"
+echo "Start hacking your skills :-)"
+
+# Keep running
+tail -f /dev/null
--- docker/ubuntu1804/docker-entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -e
-
-echo "Starting NodeJS Development environment..."
-
-echo "Started!"
-echo "Start hacking your NodeJS skills :-)"
-
-# Keep running
-tail -f /dev/null
